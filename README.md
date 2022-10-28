347c94f5d42d91be9ae890040e663437bad27be5
Farzan Soli <farzan.soleymani@gmail.com>
2022-10-28 11:28:18 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Fun_Algorithms
- Here you can find implementation of some algorithms similar to those asked in interviews.
+ Here you can find an implementation of some algorithms similar to those asked in interviews.
+
+### "getTotalEfficiency":
+In this example, you are given a list of team members' skills. You have to group these members into teams with two. There is a condition where the summation of skills in each team must be equal to others. For instance, for Skills = [4,5,4,1,2,4], we have Team_1 = [1,5] and Team_2 = [2,4]. Then, you are asked to find the sum of teams' efficiency, where efficiency = skill_1*skill_2 for each team.
+
+##### Solution: 
+I created a code that solves this problem. In the output, you are asked to provide the length of the skill list (number of members) and then provide each skill one at a time. 
+
+
+
+
+
+
+
+
+
+
+
---
+++ getTotalEfficiency.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def getTotalEfficiency(skill):
+
+    # -----------------------------------------
+    keys = np.arange(len(skill))
+    values = skill
+    hash_map = dict(zip(keys, values))
+    teams = []
+    s = []
+    for i,j in hash_map.items():
+        for n in range(i+1,len(skill)):
+            # print(n)
+            teams.append([j,hash_map[n]])
+            s.append(sum(teams[-1]))
+            unique_sum = dict(zip(s,teams))
+    # -----------------------------------------
+    Unique_teams = [list(y) for y in set(frozenset(y) for y in teams)]
+    # -----------------------------------------
+    unique_eff = []
+    complete_team = []
+    for k in unique_sum.keys():
+        for i in range(len(Unique_teams)):
+            if sum(Unique_teams[i]) == k:
+                unique_eff.append(k)
+                if len(Unique_teams[i]) == 2:
+                    complete_team.append(Unique_teams[i])
+    # -----------------------------------------
+    possible_sums = [x for x in unique_eff if unique_eff.count(x) > 1]
+    count_sums = list(set(possible_sums))
+    # -----------------------------------------
+    counts = []
+    for i in range(len(count_sums)):
+        counts.append(possible_sums.count(count_sums[i]))
+    max_count = np.max(counts)
+    max_ind = counts.index(max_count)
+    group_sums = count_sums[max_ind]
+    # -----------------------------------------
+    Groups = []
+    for i in range(len(complete_team)):
+        if sum(complete_team[i]) == group_sums:
+            Groups.append(complete_team[i])
+    # -----------------------------------------
+    effic = []
+    for i in range(len(Groups)):
+        effic.append(Groups[i][0]*Groups[i][1])
+    sum_effic = sum(effic)
+    # -----------------------------------------
+    return sum_effic
+
+
+if __name__ == '__main__':
+    
+    print("--- members count ---")
+    skill_count = int(input().strip())
+    print("--- members efficiency ---")
+    skill = []
+
+    for _ in range(skill_count):
+        skill_item = int(input().strip())
+        skill.append(skill_item)
+
+    result = getTotalEfficiency(skill)
+    print("==========")
+    print(str(result))
+    print("==========")
+
+
